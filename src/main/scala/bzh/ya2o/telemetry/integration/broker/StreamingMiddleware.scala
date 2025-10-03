28fb2c84eddbc717ca3a245791e08e75c54756fb
Yann Ollivier <yann.ollivier@magine.com>
2025-10-03 14:10:37 +0200

    Separate implementation from interface for StreamingMiddleware

--- src/main/scala/bzh/ya2o/telemetry/integration/broker/StreamingMiddleware.scala
+++ src/main/scala/bzh/ya2o/telemetry/integration/broker/StreamingMiddleware.scala
@@ -1,11 +1,6 @@
 package bzh.ya2o.telemetry
 package integration.broker
 
-import bzh.ya2o.telemetry.application.config.MiddlewareConfig
-import cats.effect.Async
-import cats.effect.Sync
-import cats.implicits._
-import fs2.concurrent.Topic
 import fs2.Stream
 import scala.{Stream => _}
 
@@ -15,25 +10,4 @@ trait StreamingMiddlewarePublisher[F[_], A] {
 
 trait StreamingMiddlewareSubscriber[F[_], A] {
   def subscribe(): Stream[F, A]
-}
-
-class StreamingMiddlewareImpl[F[_], A](topic: Topic[F, A], config: MiddlewareConfig)(implicit F: Sync[F])
-  extends StreamingMiddlewarePublisher[F, A]
-  with StreamingMiddlewareSubscriber[F, A] {
-
-  override def publish(a: A): F[Unit] = topic.publish1(a).flatMap {
-    _.fold(
-      _ => F.raiseError(new RuntimeException("Topic is closed!")),
-      F.pure
-    )
-  }
-
-  override def subscribe(): Stream[F, A] = topic.subscribe(config.maxQueued.value)
-}
-
-object StreamingMiddlewareImpl {
-  def make[F[_], A](config: MiddlewareConfig)(implicit F: Async[F]): F[StreamingMiddlewareImpl[F, A]] = {
-    Topic[F, A]
-      .map(new StreamingMiddlewareImpl[F, A](_, config))
-  }
 }
